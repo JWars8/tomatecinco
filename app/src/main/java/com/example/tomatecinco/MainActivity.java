c9b57b53a8f3c98fcffa081a16cddccc1a89371b
JWars8 <jovcventura@gmail.com>
2020-10-02 20:11:27 -0500

    con botón inciar a 2do activity

--- app/src/main/java/com/example/tomatecinco/MainActivity.java
+++ app/src/main/java/com/example/tomatecinco/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.tomatecinco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<mBoton> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void lanzar(View view){
+        Intent i=new Intent(this,MainActivity2.class);
+        startActivity(i);
     }
 }
